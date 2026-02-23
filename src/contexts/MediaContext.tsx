2ad2b9248c8a30fe74520492f24a8feff81b45df
deni-isr <denii@metropolia.fi>
2026-02-23 16:56:32 +0200

    feat: tallenna postaukset localstorageen

--- src/contexts/MediaContext.tsx
+++ src/contexts/MediaContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import type { ReactNode } from 'react';
 import type { Post } from '../types/VisualMedia';
 
@@ -11,19 +11,26 @@ interface MediaContextType {
 export const MediaContext = createContext<MediaContextType | null>(null);
 
 export const MediaProvider = ({ children }: { children: ReactNode }) => {
-  const [posts, setPosts] = useState<Post[]>([
-    {
-      id: 1,
-      type: 'image',
-      url: 'https://picsum.photos/450/450',
-      user: 'deni_dev',
-      caption: 'Testing likes!',
-      likes: 5
-    }
-  ]);
+  const [posts, setPosts] = useState<Post[]>(() => {
+    const savedPosts = localStorage.getItem('my_app_posts');
+    return savedPosts ? JSON.parse(savedPosts) : [
+      {
+        id: 1,
+        type: 'image',
+        url: 'https://picsum.photos/450/450',
+        user: 'de_dev',
+        caption: 'Tervetuloa sovellukseen!',
+        likes: 0
+      }
+    ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('my_app_posts', JSON.stringify(posts));
+  }, [posts]);
 
   const addPost = (newPost: Post) => {
-    setPosts((prev) => [...prev, newPost]);
+    setPosts((prev) => [newPost, ...prev]);
   };
 
   const handleLike = (id: number) => {
